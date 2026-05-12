c591fab92438cc5623960ddeddfa1f9205c67506
KaungSetLinn <ksl752001@gmail.com>
2026-05-12 23:54:11 +0900

    外部仕様書に合わせてAPI routeを修正、reserveBookにあたってstatus設定を改修

---
+++ library-backend/constants/reservationStatus.js
@@ -0,0 +1,9 @@
+// constants/reservationStatus.js
+
+const RESERVATION_STATUS = Object.freeze({
+    WAITING: 'WAITING',
+    RESERVED: 'RESERVED',
+    CANCELLED: 'CANCELLED',
+});
+
+module.exports = RESERVATION_STATUS;
